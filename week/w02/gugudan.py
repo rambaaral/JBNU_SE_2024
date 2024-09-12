1001a51cb5e1f75b8fa258b052e1ad495714b351
rambaaral <rambaral@jbnu.ac.kr>
2024-09-12 10:56:52 +0900

    save

---
+++ week/w02/gugudan.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/")
+def heloo():
+    return """<!DOCTYPE html>
+<html lang="kr">
+<head>
+<meta charset="UTF-8">
+<title>Flask Home Page</title>
+</head>
+<body>
+<form method="GET" action="/gugudan">
+<h2>구구단 출력하기</h2>
+<label>단 :
+<input type="text" name="dan">
+</label>
+<button type="submit">출력</button>
+</form>
+</body>
+</html>"""
+
+@app.route("/gugudan/")
+def gugudan():
+    dan = request.args.get('dan')
+    htmlstr = ""
+    for i in range(9):
+        htmlstr += f"{dan} * {i+1} = {int(dan) *(i+1)}<br>"
+    return htmlstr
+
+
+app.run(debug=True)
