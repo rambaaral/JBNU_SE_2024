3f4391c61c4bda0b6c70f1aa224df515754eddd0
rambaaral <rambaral@jbnu.ac.kr>
2024-12-07 22:48:06 +0900

    hw14_save

---
+++ hw/hw14/blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    # path('category/<str:slug>/', views.category_page),
+    path('create_post/', views.PostCreate.as_view()),
+    path('<int:pk>/', views.single_post_page, name='single_post_page'),
+    path('', views.index, name='index'),
+    path('blog_list', views.blog_list, name='blog_list'),
+    path('about_me', views.about_me, name='about_me')
+]
